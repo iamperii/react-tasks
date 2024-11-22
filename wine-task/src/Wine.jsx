f7ec5bd4ddef4c230b5ef6d3272d3562e8cfae15
Memmedova Peri <perimemmedova20052022@gmail>
2024-11-22 16:09:43 +0400

    func component added.

--- wine-task/src/Wine.jsx
+++ wine-task/src/Wine.jsx
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-export default class Wine extends Component {
-	render() {
-		return (
-			<>
-				<div className="container">
-					<h1>{this.props.title}</h1>
-					<p>{this.props.description}</p>
-					<p>{this.props.rating}</p>
-				</div>
-			</>
-		);
-	}
-}
+// export default class Wine extends Component {
+// 	render() {
+// 		return (
+// 			<>
+// 				<div className="container">
+// 					<h1>{this.props.title}</h1>
+// 					<p>{this.props.description}</p>
+// 					<p>{this.props.rating}</p>
+// 				</div>
+// 			</>
+// 		);
+// 	}
+// }
+import React from 'react';
+export const Wine = (props) => {
+	return (
+		<>
+			<div className="container">
+				<h1>{props.title}</h1>
+				<p>{props.description}</p>
+				<p>{props.rating}</p>
+			</div>
+		</>
+	);
+};
