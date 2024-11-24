057aa2e0c97ae193856814534b1e0fcfd7cd31bf
Memmedova Peri <perimemmedova20052022@gmail>
2024-11-24 22:14:48 +0400

    onMouseDown

--- tasks/src/task-form/UserLogin.jsx
+++ tasks/src/task-form/UserLogin.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './UserLogin.css';
 export default class UserLogin extends Component {
-	state = {
-		email: '',
-		password: '',
-	};
-	handleSubmit = (e) => {
-		e.preventDefault();
-		const { email, password } = this.state;
-		console.log(`Email: ${this.state.email}`);
-		console.log(`Password: ${this.state.password}`);
-	};
+	// state = {
+	// 	email: '',
+	// 	password: '',
+	// };
+	// handleSubmit = (e) => {
+	// e.preventDefault();
+	// const { email, password } = this.state;
+	// console.log(`Email: ${this.state.email}`);
+	// console.log(`Password: ${this.state.password}`);
+	// };
 	handleEmailChange = (e) => {
 		const valueEmail = e.target.value;
-		this.setState({ email: valueEmail });
+		// this.setState({ email: valueEmail });
 
 		if (!valueEmail.endsWith('.ru')) {
 			console.log('Yalnız .ru domenlərinə icazə verilir.');
@@ -21,7 +21,7 @@ export default class UserLogin extends Component {
 	};
 	handlePasswordChange = (e) => {
 		const valuePassword = e.target.value;
-		this.setState({ password: valuePassword });
+		// this.setState({ password: valuePassword });
 
 		if (valuePassword.length < 8) {
 			console.log('Ən azı 8 simvol.');
@@ -33,20 +33,22 @@ export default class UserLogin extends Component {
 				<label htmlFor="email">E-mail:</label>
 				<input
 					type="text"
-					value={this.state.email}
+					// value={this.state.email}
 					required
 					placeholder="Enter your email address"
-					onChange={this.handleEmailChange}
+					onMouseDown={this.handleEmailChange}
 				/>
 				<label htmlFor="password">Password:</label>
 				<input
 					type="password"
-					value={this.state.password}
+					// value={this.state.password}
 					required
 					placeholder="Enter your password"
-					onChange={this.handlePasswordChange}
+					onMouseDown={this.handlePasswordChange}
 				/>
-				<button type="submit" className='button'>Login</button>
+				<button type="submit" className="button">
+					Login
+				</button>
 			</form>
 		);
 	}
